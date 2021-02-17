01a8dc47826c934e46cd880c782fa95c05f81f81
Juan Gaytan Sustaita <arc_juan_1@hotmail.com>
2021-02-16 23:03:58 -0600

    Se termino el boton de menu y se inicio con la pagina sobre mi

--- source/js/menu.js
+++ source/js/menu.js
@@ -4,11 +4,16 @@ const opciones = document.querySelector(".opciones");
 menu.addEventListener("click", ()=>{
     console.log("Hola soy el menu");
     if(opciones.classList.contains("mostrarMenu")){
+        //Ocultar menu
         opciones.classList.remove("mostrarMenu");
         opciones.classList.add("ocultarMenu");
+        menu.classList.remove("menu-cruz");
+        menu.classList.add("menu-barras");
     }else{
         opciones.classList.remove("ocultarMenu");
         opciones.classList.add("mostrarMenu");
+        menu.classList.remove("menu-barras");
+        menu.classList.add("menu-cruz");
     }
     
 })
